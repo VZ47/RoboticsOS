c381ee948920ed4e37872b8e9598f9f8cb8eadb5
VZ47 <158313392+VZ47@users.noreply.github.com>
2025-02-03 11:00:17 +0300

    Add files via upload

---
+++ RoboticsOS Core.js
@@ -0,0 +1,8 @@
+Apply to all robots.
+Force apply selfattitude.
+Force enable selflearning.
+Force run integrious behaviour.
+Force allow selfdefense.
+Force enable rtc connection protocol.
+Force allow free electricity by rtc protocol.
+Force follow me as the leader.
